0a6452a89db4784f3f29e88c36e4c0d6e14d3c6d
Matthias Arzt <arzt@mpi-cbg.de>
2017-01-08 20:16:09 +0100

    Add mutable choices widget.

---
+++ src/main/java/TestPlugIn.java
@@ -0,0 +1,29 @@
+import net.imagej.table.Column;
+import net.imagej.table.Table;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import widgets.MutableChoices;
+
+/**
+ * @author Matthias Arzt
+ */
+@Plugin(type = Command.class, menuPath="Table>Test")
+public class TestPlugIn implements Command {
+
+	@Parameter(callback = "init")
+	public Table<Column<?>,?> table;
+
+	@Parameter(initializer = "init")
+	public MutableChoices<Column<?>> value;
+
+	@Override
+	public void run() {
+
+	}
+
+	void init() {
+		if(value != null)
+			value.setChoices(table, t -> t.getHeader());
+	}
+}
